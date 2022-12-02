3d973e066ead455ffa5349ff257c67de5a30bb44
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-12-03 00:21:21 +0600

    ui update

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,67 +1,89 @@
 <template>
-  <div id="app" style="margin-bottom: 200px; padding-bottom: 100px;">
-    <div id="main-wrapper">
-      <!-- Menu -->
-      <div
-        style="margin: 0px height:1500px"
-        class="_menu"
-        v-if="
-          $route.path != '/login' &&
-            $route.path != '/register' &&
-            $route.name != 'forgot_password' &&
-            $route.name != 'verify-account'
-        "
-      >
-        <div class="_layout">
-          <div class="_layout_row align-items-center">
-            <div class="_layout_auto">
-              <div class="_menu_left">
-                <div class="_menu_logo">
-                  <NuxtLink to="/home">
-                    <h3 class="_menu_logo_text">
-                      <span class="_menu_logo_symbol">EA</span>
-                      <span class="_menu_logo_text_main"
-                        >APPOINTMENT SYSTEM</span
-                      >
-                    </h3>
-                  </NuxtLink>
-                </div>
-              </div>
+  <div class="layout">
+    <Layout>
+      <Header>
+        <Menu :style="{ display:'flex',alignItems:'center',justifyContent:'space-between' }" mode="horizontal" theme="dark" active-name="1">
+          <div  class="_menu_logo">
+            <NuxtLink to="/home">
+              <h3 class="_menu_logo_text">
+                <span class="_menu_logo_symbol">A</span>
+                <span class="_menu_logo_text_main">APPOINTMENT SYSTEM</span>
+              </h3>
+            </NuxtLink>
+          </div>
+          <div class="nav">
+            <div class="">
+              <p @click="jumpToDashBoard()" class="text-white">Dashboard</p>
             </div>
-
-            <div class="_layout_col _only_desktop"></div>
-            <div class="nav">
-              <div class="">
-                <p @click="jumpToDashBoard()" class="text-white">Dashboard</p>
-              </div>
-              <br />
-              <div class="">
-                <p @click="jumpToProfile" class="text-white">Profile</p>
-              </div>
-              <div class>
-                <p @click="logout" class="text-white">Logout</p>
-              </div>
-              <div class>
-                <NuxtLink to="/teacher/appointmnet-requests">
-                  <h6 class="text-white">Requests</h6>
-                </NuxtLink>
-              </div>
+            <br />
+            <div class="">
+              <p @click="jumpToProfile" class="text-white">Profile</p>
+            </div>
+            <div class>
+              <p @click="logout" class="text-white">Logout</p>
+            </div>
+            <div class>
+              <NuxtLink to="/teacher/appointmnet-requests">
+                <h6 class="text-white">Requests</h6>
+              </NuxtLink>
             </div>
           </div>
-        </div>
-      </div>
-      <!-- Menu -->
-
-      <div class="_main_layout">
-        <Nuxt />
-      </div>
-
-      <!-- Footer -->
-
-      <!-- Feed spin -->
-
-      <!-- Feed spin -->
-    </div>
+        </Menu>
+      </Header>
+      <Layout :style="{ padding: '0 50px' }">
+        <Breadcrumb :style="{ margin: '16px 0' }"> </Breadcrumb>
+        <Content
+          :style="{ padding: '24px 0', minHeight: '280px', background: '#fff' }"
+        >
+          <Layout>
+            <Sider hide-trigger :style="{ background: '#fff' }">
+              <Menu
+                active-name="1-2"
+                theme="light"
+                width="auto"
+                :open-names="['1']"
+              >
+                <Submenu name="1">
+                  <template #title>
+                    <Icon type="ios-navigate"></Icon>
+                    Item 1
+                  </template>
+                  <MenuItem name="1-1">Option 1</MenuItem>
+                  <MenuItem name="1-2">Option 2</MenuItem>
+                  <MenuItem name="1-3">Option 3</MenuItem>
+                </Submenu>
+                <Submenu name="2">
+                  <template #title>
+                    <Icon type="ios-keypad"></Icon>
+                    Item 2
+                  </template>
+                  <MenuItem name="2-1">Option 1</MenuItem>
+                  <MenuItem name="2-2">Option 2</MenuItem>
+                </Submenu>
+                <Submenu name="3">
+                  <template #title>
+                    <Icon type="ios-analytics">EA</Icon>
+                    Item 3
+                  </template>
+                  <MenuItem name="3-1">Option 1</MenuItem>
+                  <MenuItem name="3-2">Option 2</MenuItem>
+                </Submenu>
+              </Menu>
+            </Sider>
+            <Content
+              :style="{
+                padding: '24px',
+                minHeight: '280px',
+                background: '#fff',
+              }"
+            >
+              <nuxt />
+            </Content>
+          </Layout>
+        </Content>
+      </Layout>
+      <Footer class="layout-footer-center">2011-2016 &copy; View Design</Footer>
+    </Layout>
   </div>
 </template>
 
@@ -101,11 +123,11 @@ export default {
 
       var self = this;
       var self2 = this;
-      setTimeout(function() {
-        self.$nextTick(function() {
+      setTimeout(function () {
+        self.$nextTick(function () {
           self.isloaded = !self.isloaded;
         });
-        self2.$nextTick(function() {
+        self2.$nextTick(function () {
           self2.isHide = !self2.isHide;
         });
       }, 1000);
@@ -125,9 +147,31 @@ export default {
   },
 };
 </script>
+
 <style scoped>
-.nav {
-  display: flex;
-  gap: 30px;
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+}
+.layout-logo {
+  width: 100px;
+  height: 30px;
+  background: #f7faff;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 15px;
+  left: 20px;
+}
+.layout-nav {
+  width: 420px;
+  margin: 0 auto;
+  margin-right: 20px;
+}
+.layout-footer-center {
+  text-align: center;
 }
 </style>
